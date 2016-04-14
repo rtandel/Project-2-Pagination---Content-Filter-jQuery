71e149e3ce0614af8cf5c78131e5dba9b6b6621c
Rajen Tandel <rtandel124@gmail.com>
2016-04-14 17:31:35 -0400

    Search and Comments
    The search is now working and I commented the document.

--- js/app.js
+++ js/app.js
@@ -1,119 +1,152 @@
-var $students = 0;      // Number of Students
-var $pages;             // Number of pages to accomodate the number of students.
-var $studentLimit = 10; // Amount of students allowed on each page.
+var $students = 0;                // Number of Students
+var $pages;                       // Number of pages to accomodate the number of students.
+var $studentLimit = 10;           // Amount of students allowed on each page.
 var $list = $(".student-item");   // The list items used to count number of students.
+var $foundStudentsArray = [];     // The array used to keep track of the search results.
 
 // Calculates the number of students.
-$list.each(function() {
-  $students++;
-  $(this).addClass("student_" + $students);
-});
-
-// Calculates the number of pages required to accomodate the number of students.
-$pages = Math.ceil($students / $studentLimit);
-
-
-console.log($students);
-console.log($pages);
-
-  // Creates the list of available pages.
-  $(".page").append("<div class=\"pagination\"><ul></ul></div>");
-
-
-$pageCreation = 0;  // Provides a counter to create the right number of pages.
-// Creates the list items for each page.
-while ($pages !== $pageCreation) {
-  $pageCreation++;
-  $(".pagination ul").append("<li><a class=\"page_"+$pageCreation+"\">"+$pageCreation+"</a></li>");
+function calculateStudents() {
+  $(".student-item:visible").each(function() {
+      $students++;
+  });
 }
 
-
-
-// Initially hides all of the list items.
+// Hides all of the list items.
 function hideAll() {
   $list.each(function() {
     $(this).hide();
   });
 }
 
-// Initially, the first list item is declared as active
-$(".pagination .page_1").addClass("active");
-correctStudents();
+// Shows all of the list items.
+function showAll() {
+  $list.each(function() {
+    $(this).show();
+  });
+}
 
-// Hides all of the items Selects the specified
-$(".pagination a").click(function() {
-  $(".pagination .active").removeClass("active");
-  $(this).addClass("active");
-  $pageNumber = $(this).text();
-  correctStudents();
-});
+calculateStudents();   // to initially count the number of students.
+hideAll();             // to hide all of the students intially.
+
+// Calculates the number of pages required to accomodate the number of students.
+$pages = Math.ceil($students / $studentLimit);
 
 
-// Chooses the set of students based on which page is selected.
-function correctStudents() {
+// appends the div and the unordered list to the page
+$(".page").append("<div class=\"pagination\"><ul></ul></div>");
+
+ // Provides a counter to create the right number of pages.
+// Creates the list items for each page.
+// This also sets the action listener for the pages.
+function pageCreate() {
+  // If a page list already exists.
+  if ($(".pagination ul").length !== 0) {
+    $(".pagination ul").empty();
+  }
+  $pageCreation = 0;       // counter to stop the while loop when it reaches
+                           // the correct amount of page numbers.
+  console.log($pages);
+  // Creates the correct number of pages.
+  while ($pages !== $pageCreation) {
+    $pageCreation++;
+    $(".pagination ul").append("<li><a>"+$pageCreation+"</a></li>");
+    $(".pagination li:first").addClass("active");
+    console.log($pageCreation);
+  }
+  // Specifies the action listener each time this function is called.
+  $(".pagination a").click(function() {
+    $(".pagination .active").removeClass("active");
+    $(this).addClass("active");
+    console.log($pages);
+    var $selected = Number($(this).text());
+    paginateInitial($selected);
+  });
+}
+
+// Initially calls the functions.
+hideAll();
+pageCreate();
+$(".pagination li:first").addClass("active");
+paginateInitial(1);
+
+
+// pagination for the initial items.
+function paginateInitial(x) {
+    hideAll();
+      if (x > $pages.length) {
+        x = x / 10;
+      }
+      var $page = x;
+      var $currentMax = 10 * $page - 1;
+      var $min = $currentMax - 9;
+      if ($page === $pages.length) {
+        $currentMax = $students;
+      }
+      if ($(".pagination .active").text() === ""+$page+"") {
+        hideAll();
+        for (var i = $min; i <= $currentMax; i++) {
+          console.log($min);
+          $('.student-item').eq(i).show();
+        }
+      }
+}
+
+// pagination for the search items.
+function paginateSearch(y) {
   hideAll();
-  if ($(".pagination .page_1").hasClass("active")) {
-    for (var i = 1; i < 11; i++) {
-      $(".student_" + i).show();
-    }
+  var $index;                           // The place in the array.
+  pageCreate($pages);                   // Creates the pages.
+  var $page = y;                        // Specifies the page number
+  var $currentMax = 10 * $page - 1;     // Sets the max based on which page.
+  var $min = $currentMax - 9;           // Min calculated based on the max.
+
+  // if the page number is the last page, then paginate all of the items.
+  if ($page === $pages.length) {
+    $currentMax = $foundStudentsArray.length;
   }
-  if ($(".pagination .page_2").hasClass("active")) {
-    for (var i = 11; i < 21; i++) {
-      $(".student_" + i).show();
-    }
-  }
-  if ($(".pagination .page_3").hasClass("active")) {
-    for (var i = 21; i < 31; i++) {
-      $(".student_" + i).show();
-    }
-  }
-  if ($(".pagination .page_4").hasClass("active")) {
-    for (var i = 31; i < 41; i++) {
-      $(".student_" + i).show();
-    }
-  }
-  if ($(".pagination .page_5").hasClass("active")) {
-    for (var i = 41; i < 51; i++) {
-      $(".student_" + i).show();
-    }
-  }
-  if ($(".pagination .page_6").hasClass("active")) {
-    for (var i = 51; i < 55; i++) {
-      $(".student_" + i).show();
+  //
+  if ($(".pagination .active").text() === ""+$page+"") {
+    hideAll();
+    for (var i = $min; i <= $currentMax; i++) {
+      $index = $foundStudentsArray[i];
+      console.log($min);
+      $('.student-item').eq($index).show();
     }
   }
 }
 
-//Now to get the search working.
-
+// Append the search div, search box, and button.
 $(".page-header").append("<div class=\"student-search\">" +
                           "<input placeholder=\"Search for students...\">" +
                           "<button class=\"search-button\">Search</button></div>");
+// The text input from the user.
+var $searchText = $(".student-search input");
 
+// Function called when the search button is clicked.
 $(".search-button").click(function() {
-  var $searchText = $(".student-search input").val();
-
-    // for (var i = 1; i < $students; i++) {
-    //   if ($(".student_"+ i + " h3").text() == $searchText) {
-    //     hideAll();
-    //     $(".student_" + i).show();
-    //     break;
-    //   }
-    // }
-    // for (var i = 1; i < $students; i++) {
-    //   if ($(".student_"+ i +" .email").text() === $searchText) {
-    //     hideAll();
-    //     $(".student_" + i).show();
-    //     break;
-    //   }
-    // }
-    var $counter = 0;
-    hideAll();
-    $list.each(function() {
-      $("li:contains("+ $searchText+")").show();
+  $foundStudentsArray = [];
+  $searchText = $(".student-search input").val();
+  // If the search box is empty, reset.
+  if ($searchText === '') {
+    showAll();
+    calculateStudents();
+    pageCreate();
+  }
+    hideAll();              // Hides the items before comparing.
+    var $counter;           // Keeps track of the items found in the search.
+    // if the item is found, add it to the array.
+    $(".student-list li:contains("+ $searchText+")").each(function() {
+        $counter = $(this).index();
+        $foundStudentsArray.push($counter);
+        console.log($counter);
     });
+    // Recalculates the pages with the amount of matched items that were
+    // added to the array.
+    $pages = Math.ceil($foundStudentsArray.length / 10);
+    paginateSearch($(".pagination .active").text());
+});
 
-
-
-
+// Triggers the click function when the input text is changed
+$($searchText).change(function() {
+  $(".search-button").trigger("click");
 });
